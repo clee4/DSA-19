38f4aa9d7fb9feccb3bbfc2b86a1b54352f6f3db
Chris Lee <clee4@olin.edu>
2019-01-29 19:58:22 -0500

    Adding changes to day 1

--- datastructures/day01/src/MyArrayList.java
+++ datastructures/day01/src/MyArrayList.java
@@ -2,41 +2,48 @@ public class MyArrayList {
     private Cow[] elems;
     private int size;
 
-    // TODO: Runtime: O(?)
+    // TODO: Runtime: O(1)
     public MyArrayList() {
         // TODO
+        this.elems = new Cow[10]
     }
 
-    // TODO: Runtime: O(?)
+    // TODO: Runtime: O(1)
     public MyArrayList(int capacity) {
         // TODO
+        this.elems = new Cow[capacity]
     }
 
-    // TODO: Runtime: O(?)
+    // TODO: Runtime: O(1)
     public void add(Cow c) {
-        // TODO
+        elems[i] = c;
+        size+=1
     }
 
-    // TODO: Runtime: O(?)
+    // TODO: Runtime: O(1)
     public int size() {
-        // TODO
-        return -1;
+        return size;
     }
 
-    // TODO: Runtime: O(?)
+    // TODO: Runtime: O(1)
     public Cow get(int index) {
-        // TODO
-        return null;
+        return elems[i];
     }
 
-    // TODO: Runtime: O(?)
+    // TODO: Runtime: O(n)
     public Cow remove(int index) {
-        // TODO
+        for (int i = index; i<elems.length(); i++){
+            elems[i] = elems[i+1]
+        }
         return null;
     }
 
     // TODO: Runtime: O(?)
     public void add(int index, Cow c) {
         // TODO
+        for (int i = elems.length()-1; i>=index; i--){
+            elems[i] = elems[i+1]
+        }
+        elems[index] = c
     }
 }
